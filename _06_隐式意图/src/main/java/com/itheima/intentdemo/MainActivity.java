cb4e275279cd8d9ff8288dc80f9fe2f0c08b7d21
Will <562165195@qq.com>
2017-11-20 00:14:16 +0800

    先判断是不是存在相应的界面,避免直接启动导致程序的creash

--- _06_隐式意图/src/main/java/com/itheima/intentdemo/MainActivity.java
+++ _06_隐式意图/src/main/java/com/itheima/intentdemo/MainActivity.java
@@ -2,29 +2,37 @@ package com.itheima.intentdemo;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
-	public void click(View view){
-		Intent intent = new Intent();
-		//action 动作
-		//泡茶 泡妞
-		intent.setAction("com.itheima.intentdemo.XXXX");
-		intent.addCategory(Intent.CATEGORY_DEFAULT);
+    public void click(View view) {
+        Intent intent = new Intent();
+        //action 动作
+        //泡茶 泡妞
+        intent.setAction("com.itheima.intentdemo.XXXX");
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
 //		intent.setData(Uri.parse("itheima://afaf"));
 //		intent.setType("text/plain");
-		intent.setDataAndType(Uri.parse("itheima://afaf"), "text/plain");
-		intent.putExtra("body","我是隐式意图携带过来的数据");
-		startActivity(intent);
-	}
+        intent.setDataAndType(Uri.parse("itheima://afaf"), "text/plain");
+        intent.putExtra("body", "我是隐式意图携带过来的数据");
+
+        PackageManager packageManager = getPackageManager();
+        ResolveInfo resolveInfo = packageManager.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        if (null != resolveInfo) {
+            startActivity(intent);
+        }
+
+    }
 
 }
